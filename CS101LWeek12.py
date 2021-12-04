dd88e242874687c85e03d11d9e1cc4b68761d252
linesonlines <91343837+linesonlines@users.noreply.github.com>
2021-12-03 20:54:15 -0600

    Add files via upload

---
+++ CS101LWeek12.py
@@ -0,0 +1,85 @@
+#Lab 14
+#Captain Daryani
+#cpd6g3@umsystem.edu
+
+import turtle
+
+class Point(object):
+
+    def __init__(self, x, y, color):
+        self.x = x
+        self.y = y
+        self.color = color
+
+    def draw(self):
+        turtle.penup()
+        turtle.goto(self.x, self.y)
+        turtle.pendown()
+        turtle.color(self.color)
+        turtle.setheading(0)
+        self.draw_action()
+
+    def draw_action(self):
+        turtle.dot()
+
+class Circle(Point):
+
+    def __init__(self, x1, y1, radius, color):
+        super().__init__(x1, y1, color)
+        self.radius = radius
+
+    def draw_action(self):
+        turtle.circle(self.radius)
+
+class CircleFilled(Circle):
+
+    def __init__(self, x1, y1, radius, color, fillcolor):
+        super().__init__(x1, y1, radius, color)
+        self.fillcolor = fillcolor
+
+    def draw_action(self):
+        turtle.fillcolor(self.fillcolor)
+        turtle.begin_fill()
+        Circle.draw_action(self)
+        turtle.end_fill()
+    
+
+class Box(Point):
+
+    def __init__(self, x1, y1, width, height, color):
+        super().__init__(x1, y1, color)
+        self.width = width
+        self.height = height
+
+    def draw_action(self):
+        turtle.forward(self.width)
+        turtle.right(90)
+        turtle.forward(self.height)
+        turtle.right(90)
+        turtle.forward(self.width)
+        turtle.right(90)
+        turtle.forward(self.height)
+
+class BoxFilled(Box):
+
+    def __init__(self, x1, y1, width, height, color, fillcolor):
+        super().__init__(x1, y1, width, height, color)
+        self.fillcolor = fillcolor
+
+    def draw_action(self):
+        turtle.fillcolor(self.fillcolor)
+        turtle.begin_fill()
+        Box.draw_action(self)
+        turtle.end_fill()
+    
+b = Box(-100, 100, 50, 20, "orange")
+bf = BoxFilled(1, 2, 100, 200, "red", "blue")
+c = Circle(-200, -200, 100, "green")
+cf = CircleFilled(60, 60, 100, "purple", "yellow")
+
+b.draw()
+bf.draw()
+c.draw()
+cf.draw()
+
+
