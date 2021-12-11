9b325f7813ce3cfdae6bcab4f56fcf99c8b8ec64
linesonlines <91343837+linesonlines@users.noreply.github.com>
2021-12-10 19:30:21 -0600

    Add files via upload

---
+++ CS101LWeek13.py
@@ -0,0 +1,42 @@
+import unittest
+import Grades
+import math
+
+# Captain Daryani
+# cpd6g3@umsystem.edu
+
+class Grade_Test(unittest.TestCase):
+
+    def test_total_returns_total_of_list(self):
+        result = Grades.total([1, 10, 22])
+        self.assertEqual(result, 33, "The total function should return 33")
+
+    def test_total_returns_0(self):
+        result = Grades.total([])
+        self.assertEqual(result, 0, "The total function should return 0")
+
+    def test_average_one(self):
+        result = Grades.average([2, 5, 9])
+        self.assertAlmostEqual(result, 5.33333, 5)
+
+    def test_average_two(self):
+        result = Grades.average([2, 15, 22, 9])
+        self.assertAlmostEqual(result, 12.0000, 4)
+
+    def test_average_returns_nan(self):
+        result = Grades.average([])
+        self.assertIs(result, math.nan)
+
+    def test_median_a(self):
+        result = Grades.median([2, 5, 1])
+        self.assertEqual(result, 2, "The median function should return 2")
+
+    def test_median_b(self):
+        result = Grades.median([5, 2, 1, 3] )
+        self.assertEqual(result, 2.5, "The median function should return 2.5")
+
+    def test_median_empty_list(self):
+        with self.assertRaises(ValueError):
+            result = Grades.median([])
+        
+unittest.main()
